7be25c7c1d16f36fc71f8d159da633e535558c54
agilestarskim <agilestarskim@gmail.com>
2024-12-13 00:37:11 +0900

    오타 수정

--- src/components/layout/Navigation/Navigation.js
+++ src/components/layout/Navigation/Navigation.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
-import * as styles from "./navigation.module.css"
+import * as styles from "./Navigation.module.css"
 
 const Navigation = () => {
   return (
